d26221c82a81628ceb73c8c57f3e4c72f728ac47
hungerlinknaresh <94264622+hungerlinknaresh@users.noreply.github.com>
2022-08-16 22:12:33 +0530

    react-app fetchdata

---
+++ src/component/CardData.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+
+class CardData extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      datalist: [],
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then(
+        (datalist) => {
+          this.setState({ datalist: datalist });
+        },
+        (error) => {
+          alert(error);
+        }
+      );
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          <h1 style={{textAlign:'center'}}>Card Data</h1>
+          <Container>
+            <Row
+              className="justify-content-md-center"
+              style={{ margin: "30px", padding: "10px" }}
+            >
+              {this.state.datalist.map((data, id) => (
+                <Card key={data.id}>
+                  <Card.Body>
+                    <Card.Title>{data.title}</Card.Title>
+                    <Card.Text>{data.body}</Card.Text>
+                  </Card.Body>
+                </Card>
+              ))}
+            </Row>
+          </Container>
+        </div>
+      </>
+    );
+  }
+}
+
+export default CardData;
---
+++ src/component/TableData.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Table from "react-bootstrap/Table";
+
+const TableData = () => {
+  const [datalist, setDatalist] = useState(null);
+  useEffect(() => {
+    getemployees();
+  }, []);
+
+  const getemployees = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setDatalist(result);
+        },
+        (error) => {
+          setDatalist(null);
+          alert(error);
+        }
+      );
+  };
+  if (!datalist) return <div>No Record Found</div>;
+  const handleDelete = (id, e) => {
+    setDatalist(datalist.filter((v, i) => i !== id));
+  };
+  return (
+    <div>
+      <h1 style={{textAlign:'center', margin:'10px'}}>List Data</h1>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Body</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {datalist.map((data, id) => (
+            <tr key={data.id}>
+              <td>{data.id}</td>
+              <td>{data.title}</td>
+              <td>{data.body}</td>
+              <td>
+                <Button variant="danger" onClick={(e) => handleDelete(id, e)}>
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+export default TableData;
